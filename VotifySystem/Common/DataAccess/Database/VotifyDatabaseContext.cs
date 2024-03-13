4f1cfe14f2fe4d0f5016292de3b2e154af7b89ba
Freddie Mason <fmason@redev.co.uk>
2024-03-13 09:05:21 +0000

    started db implementation

---
+++ VotifySystem/Common/DataAccess/Database/VotifyDatabaseContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VotifySystem.Common.Classes;
+
+namespace VotifyDataAccess.Database;
+
+/// <summary>
+/// EFCore with SqLite
+/// https://entityframeworkcore.com/providers-sqlite
+/// </summary>
+public class VotifyDatabaseContext : DbContext
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="options"></param>
+    public VotifyDatabaseContext(DbContextOptions<VotifyDatabaseContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Administrator> Administrators { get; set; }
+    public DbSet<Candidate> Candidates { get; set; }
+    public DbSet<Constituency> Constituencies { get; set; }
+    public DbSet<ElectionCandidate> ElectionCandidates { get; set; }
+    public DbSet<Voter> Voters { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="optionsBuilder"></param>
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=CustomerDB.db;");
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="modelBuilder"></param>
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Administrator>().ToTable(nameof(Administrator));
+        modelBuilder.Entity<Candidate>().ToTable(nameof(Candidate));
+        modelBuilder.Entity<Constituency>().ToTable(nameof(Constituency));
+        modelBuilder.Entity<ElectionCandidate>().ToTable(nameof(ElectionCandidates));
+        modelBuilder.Entity<Voter>().ToTable(nameof(Voter));
+    }
+}
