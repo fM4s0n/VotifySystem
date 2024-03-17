71bcf51164ab8e091dc1e7cb58f696083e7d68de
fM4s0n <115559911+fM4s0n@users.noreply.github.com>
2024-03-17 10:23:10 +0000

    started register for election form

--- VotifySystem/InPerson/Forms/frmRegisterForElection.Designer.cs
+++ VotifySystem/InPerson/Forms/frmRegisterForElection.Designer.cs
@@ -28,11 +28,104 @@ partial class frmRegisterForElection
     /// </summary>
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "frmRegisterForElection";
+        cmbElections = new ComboBox();
+        lblSelectElection = new Label();
+        cmbConstituencies = new ComboBox();
+        lblSelectConstituency = new Label();
+        lblRegisterForElection = new Label();
+        btnRegister = new Button();
+        btnCancel = new Button();
+        SuspendLayout();
+        // 
+        // cmbElections
+        // 
+        cmbElections.FormattingEnabled = true;
+        cmbElections.Location = new Point(156, 95);
+        cmbElections.Name = "cmbElections";
+        cmbElections.Size = new Size(287, 23);
+        cmbElections.TabIndex = 0;
+        // 
+        // lblSelectElection
+        // 
+        lblSelectElection.AutoSize = true;
+        lblSelectElection.Location = new Point(36, 98);
+        lblSelectElection.Name = "lblSelectElection";
+        lblSelectElection.Size = new Size(86, 15);
+        lblSelectElection.TabIndex = 1;
+        lblSelectElection.Text = "Select Election:";
+        // 
+        // cmbConstituencies
+        // 
+        cmbConstituencies.FormattingEnabled = true;
+        cmbConstituencies.Location = new Point(156, 138);
+        cmbConstituencies.Name = "cmbConstituencies";
+        cmbConstituencies.Size = new Size(287, 23);
+        cmbConstituencies.TabIndex = 2;
+        // 
+        // lblSelectConstituency
+        // 
+        lblSelectConstituency.AutoSize = true;
+        lblSelectConstituency.Location = new Point(36, 141);
+        lblSelectConstituency.Name = "lblSelectConstituency";
+        lblSelectConstituency.Size = new Size(114, 15);
+        lblSelectConstituency.TabIndex = 3;
+        lblSelectConstituency.Text = "Select Constituency:";
+        // 
+        // lblRegisterForElection
+        // 
+        lblRegisterForElection.AutoSize = true;
+        lblRegisterForElection.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        lblRegisterForElection.Location = new Point(95, 18);
+        lblRegisterForElection.Name = "lblRegisterForElection";
+        lblRegisterForElection.Size = new Size(289, 37);
+        lblRegisterForElection.TabIndex = 4;
+        lblRegisterForElection.Text = "Register for an Election";
+        // 
+        // btnRegister
+        // 
+        btnRegister.Location = new Point(391, 219);
+        btnRegister.Name = "btnRegister";
+        btnRegister.Size = new Size(75, 23);
+        btnRegister.TabIndex = 5;
+        btnRegister.Text = "Register";
+        btnRegister.UseVisualStyleBackColor = true;
+        btnRegister.Click += btnRegister_Click;
+        // 
+        // btnCancel
+        // 
+        btnCancel.Location = new Point(12, 219);
+        btnCancel.Name = "btnCancel";
+        btnCancel.Size = new Size(75, 23);
+        btnCancel.TabIndex = 6;
+        btnCancel.Text = "Cancel";
+        btnCancel.UseVisualStyleBackColor = true;
+        btnCancel.Click += btnCancel_Click;
+        // 
+        // frmRegisterForElection
+        // 
+        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(478, 254);
+        Controls.Add(btnCancel);
+        Controls.Add(btnRegister);
+        Controls.Add(lblRegisterForElection);
+        Controls.Add(lblSelectConstituency);
+        Controls.Add(cmbConstituencies);
+        Controls.Add(lblSelectElection);
+        Controls.Add(cmbElections);
+        Name = "frmRegisterForElection";
+        Text = "frmRegisterForElection";
+        ResumeLayout(false);
+        PerformLayout();
     }
 
     #endregion
+
+    private ComboBox cmbElections;
+    private Label lblSelectElection;
+    private ComboBox cmbConstituencies;
+    private Label lblSelectConstituency;
+    private Label lblRegisterForElection;
+    private Button btnRegister;
+    private Button btnCancel;
 }
--- VotifySystem/InPerson/Forms/frmRegisterForElection.cs
+++ VotifySystem/InPerson/Forms/frmRegisterForElection.cs
@@ -1,18 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using VotifySystem.Common.BusinessLogic.Services;
 
 namespace VotifySystem.InPerson.Forms;
+
+/// <summary>
+/// Form for a voter to register to be able to vote in an Election
+/// </summary>
 public partial class frmRegisterForElection : Form
 {
-    public frmRegisterForElection()
+    IUserService? _userService;
+    public frmRegisterForElection(IUserService userService)
     {
         InitializeComponent();
+
+        if (DesignMode)
+            return;
+
+        _userService = userService;
+
+        Init();
+    }
+
+    private void Init()
+    {
+
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void btnRegister_Click(object sender, EventArgs e)
+    {
+        if (ValidateInput() == false)
+            return;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    private bool ValidateInput()
+    {
+        bool success = true;
+        foreach (ComboBox cmb in new List<ComboBox> { cmbElections, cmbElections })
+        {
+            if (cmb.SelectedIndex == -1)
+            {
+                cmb.BackColor = Color.Red;
+                success = false;
+            }
+            else
+            {
+                cmb.BackColor = Color.White;
+            }
+        }
+
+        return success;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void btnCancel_Click(object sender, EventArgs e)
+    {
+
     }
 }
