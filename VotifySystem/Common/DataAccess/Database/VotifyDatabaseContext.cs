0444eebc4aadadb68ed185812cc218772a99db5e
fM4s0n <115559911+fM4s0n@users.noreply.github.com>
2024-03-17 13:59:28 +0000

    amended dbContext to primary constructor

--- VotifySystem/Common/DataAccess/Database/VotifyDatabaseContext.cs
+++ VotifySystem/Common/DataAccess/Database/VotifyDatabaseContext.cs
@@ -7,31 +7,17 @@ namespace VotifyDataAccess.Database;
 /// EFCore with SqLite
 /// https://entityframeworkcore.com/providers-sqlite
 /// </summary>
-public class VotifyDatabaseContext : DbContext
+/// <remarks>
+/// 
+/// </remarks>
+/// <param name="options"></param>
+public class VotifyDatabaseContext(DbContextOptions<VotifyDatabaseContext> options) : DbContext(options)
 {
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="options"></param>
-    public VotifyDatabaseContext(DbContextOptions<VotifyDatabaseContext> options) : base(options)
-    {
-        
-    }
-
     public DbSet<Administrator> Administrators { get; set; }
     public DbSet<Candidate> Candidates { get; set; }
     public DbSet<Constituency> Constituencies { get; set; }
     public DbSet<ElectionCandidate> ElectionCandidates { get; set; }
     public DbSet<Voter> Voters { get; set; }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="optionsBuilder"></param>
-    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    //{
-    //    optionsBuilder.UseSqlite("Data Source=VotifyDB.db;");
-    //}
 
     /// <summary>
     /// 
