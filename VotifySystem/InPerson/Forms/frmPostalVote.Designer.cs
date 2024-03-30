25193ee101cf9e2ab0a7af3ccb599abb7e2553fa
fM4s0n <115559911+fM4s0n@users.noreply.github.com>
2024-03-30 21:53:52 +0000

    Did postal vote form

--- VotifySystem/InPerson/Forms/frmPostalVote.Designer.cs
+++ VotifySystem/InPerson/Forms/frmPostalVote.Designer.cs
@@ -29,6 +29,17 @@ partial class frmPostalVote
     private void InitializeComponent()
     {
         lblPostalVotes = new Label();
+        cmbElection = new ComboBox();
+        cmbCandidate = new ComboBox();
+        lblSelectCandidate = new Label();
+        lblSelectElection = new Label();
+        lblSelectCountry = new Label();
+        cmbCountry = new ComboBox();
+        txtVotesToAdd = new TextBox();
+        lblAddVotes = new Label();
+        btnSubmit = new Button();
+        btnReset = new Button();
+        btnCancel = new Button();
         SuspendLayout();
         // 
         // lblPostalVotes
@@ -41,11 +52,124 @@ partial class frmPostalVote
         lblPostalVotes.TabIndex = 0;
         lblPostalVotes.Text = "Submit Postal Votes";
         // 
+        // cmbElection
+        // 
+        cmbElection.Enabled = false;
+        cmbElection.FormattingEnabled = true;
+        cmbElection.Location = new Point(66, 144);
+        cmbElection.Name = "cmbElection";
+        cmbElection.Size = new Size(371, 23);
+        cmbElection.TabIndex = 2;
+        cmbElection.SelectedIndexChanged += cmbElection_SelectedIndexChanged;
+        // 
+        // cmbCandidate
+        // 
+        cmbCandidate.Enabled = false;
+        cmbCandidate.FormattingEnabled = true;
+        cmbCandidate.Location = new Point(66, 203);
+        cmbCandidate.Name = "cmbCandidate";
+        cmbCandidate.Size = new Size(371, 23);
+        cmbCandidate.TabIndex = 3;
+        // 
+        // lblSelectCandidate
+        // 
+        lblSelectCandidate.AutoSize = true;
+        lblSelectCandidate.Location = new Point(66, 185);
+        lblSelectCandidate.Name = "lblSelectCandidate";
+        lblSelectCandidate.Size = new Size(98, 15);
+        lblSelectCandidate.TabIndex = 3;
+        lblSelectCandidate.Text = "Select Candidate:";
+        // 
+        // lblSelectElection
+        // 
+        lblSelectElection.AutoSize = true;
+        lblSelectElection.Location = new Point(66, 126);
+        lblSelectElection.Name = "lblSelectElection";
+        lblSelectElection.Size = new Size(86, 15);
+        lblSelectElection.TabIndex = 4;
+        lblSelectElection.Text = "Select Election:";
+        // 
+        // lblSelectCountry
+        // 
+        lblSelectCountry.AutoSize = true;
+        lblSelectCountry.Location = new Point(69, 70);
+        lblSelectCountry.Name = "lblSelectCountry";
+        lblSelectCountry.Size = new Size(87, 15);
+        lblSelectCountry.TabIndex = 5;
+        lblSelectCountry.Text = "Select Country:";
+        // 
+        // cmbCountry
+        // 
+        cmbCountry.FormattingEnabled = true;
+        cmbCountry.Location = new Point(66, 88);
+        cmbCountry.Name = "cmbCountry";
+        cmbCountry.Size = new Size(371, 23);
+        cmbCountry.TabIndex = 1;
+        cmbCountry.SelectedIndexChanged += cmbCountry_SelectedIndexChanged;
+        // 
+        // txtVotesToAdd
+        // 
+        txtVotesToAdd.Enabled = false;
+        txtVotesToAdd.Location = new Point(66, 272);
+        txtVotesToAdd.Name = "txtVotesToAdd";
+        txtVotesToAdd.Size = new Size(371, 23);
+        txtVotesToAdd.TabIndex = 6;
+        // 
+        // lblAddVotes
+        // 
+        lblAddVotes.AutoSize = true;
+        lblAddVotes.Location = new Point(69, 254);
+        lblAddVotes.Name = "lblAddVotes";
+        lblAddVotes.Size = new Size(140, 15);
+        lblAddVotes.TabIndex = 7;
+        lblAddVotes.Text = "Enter Postal Votes to add:";
+        // 
+        // btnSubmit
+        // 
+        btnSubmit.Location = new Point(362, 323);
+        btnSubmit.Name = "btnSubmit";
+        btnSubmit.Size = new Size(75, 23);
+        btnSubmit.TabIndex = 8;
+        btnSubmit.Text = "Submit";
+        btnSubmit.UseVisualStyleBackColor = true;
+        btnSubmit.Click += btnSubmit_Click;
+        // 
+        // btnReset
+        // 
+        btnReset.Location = new Point(264, 323);
+        btnReset.Name = "btnReset";
+        btnReset.Size = new Size(75, 23);
+        btnReset.TabIndex = 9;
+        btnReset.Text = "Reset Form";
+        btnReset.UseVisualStyleBackColor = true;
+        btnReset.Click += btnReset_Click;
+        // 
+        // btnCancel
+        // 
+        btnCancel.Location = new Point(69, 323);
+        btnCancel.Name = "btnCancel";
+        btnCancel.Size = new Size(75, 23);
+        btnCancel.TabIndex = 10;
+        btnCancel.Text = "Cancel";
+        btnCancel.UseVisualStyleBackColor = true;
+        btnCancel.Click += btnCancel_Click;
+        // 
         // frmPostalVote
         // 
         AutoScaleDimensions = new SizeF(7F, 15F);
         AutoScaleMode = AutoScaleMode.Font;
-        ClientSize = new Size(525, 450);
+        ClientSize = new Size(490, 378);
+        Controls.Add(btnCancel);
+        Controls.Add(btnReset);
+        Controls.Add(btnSubmit);
+        Controls.Add(lblAddVotes);
+        Controls.Add(txtVotesToAdd);
+        Controls.Add(cmbCountry);
+        Controls.Add(lblSelectCountry);
+        Controls.Add(lblSelectElection);
+        Controls.Add(lblSelectCandidate);
+        Controls.Add(cmbCandidate);
+        Controls.Add(cmbElection);
         Controls.Add(lblPostalVotes);
         Name = "frmPostalVote";
         Text = "frmPostalVote";
@@ -56,4 +180,15 @@ partial class frmPostalVote
     #endregion
 
     private Label lblPostalVotes;
+    private ComboBox cmbElection;
+    private ComboBox cmbCandidate;
+    private Label lblSelectCandidate;
+    private Label lblSelectElection;
+    private Label lblSelectCountry;
+    private ComboBox cmbCountry;
+    private TextBox txtVotesToAdd;
+    private Label lblAddVotes;
+    private Button btnSubmit;
+    private Button btnReset;
+    private Button btnCancel;
 }
--- VotifySystem/InPerson/Forms/frmPostalVote.cs
+++ VotifySystem/InPerson/Forms/frmPostalVote.cs
@@ -1,4 +1,8 @@
-﻿using VotifySystem.Common.BusinessLogic.Services;
+﻿using System.Xml.Serialization;
+using VotifySystem.Common.BusinessLogic.Helpers;
+using VotifySystem.Common.BusinessLogic.Services;
+using VotifySystem.Common.Classes;
+using VotifySystem.Common.Classes.Elections;
 using VotifySystem.Common.DataAccess.Database;
 
 namespace VotifySystem.InPerson.Forms;
@@ -11,15 +15,154 @@ public partial class frmPostalVote : Form
     private readonly IUserService _userService;
     private readonly IDbService _dbService;
 
+    private List<Election> _elections = [];
+    private List<Candidate> _candidates = [];
+
     public frmPostalVote(IUserService userService, IDbService dbService)
     {
         InitializeComponent();
 
-        if (DesignMode)        
+        if (DesignMode)
             return;
-        
+
         _userService = userService;
         _dbService = dbService;
 
+        Init();
+    }
+
+    private void Init()
+    {
+        if (_userService.GetCurrentUserLevel() != UserLevel.Administrator)
+        {
+            MessageBox.Show("You do not have permission to access this page");
+            Close();
+        }
+
+        foreach (Country country in Enum.GetValues(typeof(Country)))
+            cmbCountry.Items.Add(country);
+    }
+
+    /// <summary>
+    /// loads elections into cmbElection based on country selected
+    /// </summary>
+    private void cmbCountry_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (cmbCountry.SelectedIndex == -1)
+            return;
+
+        _elections = _dbService!.GetDatabaseContext().Elections.Where(e => e.Country == (Country)cmbCountry.SelectedItem).ToList();
+
+        if (_elections.Count == 0)
+        {
+            MessageBox.Show("No elections found for this country", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            Close();
+        }
+
+        InitElectionComboBox();
+
+        cmbCountry.Enabled = false;
+    }
+
+    /// <summary>
+    /// Loads candidates based on value selected in Election combo box 
+    /// </summary>
+    private void cmbElection_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (cmbElection.SelectedIndex == -1)
+            return;
+
+        _candidates = _dbService!.GetDatabaseContext().Candidates.Where(c => c.ElectionId == cmbElection.SelectedValue.ToString()).ToList();
+
+        InitCandidateComboBox();
+
+        cmbElection.Enabled = false;
+    }
+
+    private void InitElectionComboBox()
+    {
+        cmbElection.DataSource = null;
+        cmbElection.DisplayMember = "Description";
+        cmbElection.ValueMember = "ElectionId";
+        cmbElection.DataSource = _elections;
+    }
+
+    private void InitCandidateComboBox()
+    {
+        cmbCandidate.DataSource = null;
+        cmbCandidate.DisplayMember = "FullName";
+        cmbCandidate.ValueMember = "Id";
+        cmbCandidate.DataSource = _candidates;
+    }
+
+    private void btnCancel_Click(object sender, EventArgs e)
+    {
+        Close();
+    }
+
+    private void btnReset_Click(object sender, EventArgs e)
+    {
+        ResetForm();
+    }
+
+    private void ResetForm()
+    {
+        _elections.Clear();
+        _candidates.Clear();
+        cmbCountry.SelectedIndex = -1;
+        cmbElection.DataSource = null;
+        cmbElection.SelectedIndex = -1;
+        cmbCandidate.SelectedIndex = -1;
+        cmbCandidate.DataSource = null;
+        txtVotesToAdd.Text = string.Empty;
+    }
+
+    private void btnSubmit_Click(object sender, EventArgs e)
+    {
+        if (cmbCandidate.SelectedIndex != -1)
+        {
+            MessageBox.Show("Please select a candidate", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            return;
+        }
+
+        Candidate candidate = _candidates.First(c => c.Id == cmbCandidate.SelectedValue.ToString());
+
+        if (candidate == null) 
+        {
+            cmbCandidate.SelectedIndex = -1;
+            InitCandidateComboBox();
+            return;
+        }
+
+        if (ValidateVoteInput())
+        {
+            candidate.AddVotes(int.Parse(cmbCandidate.Text));
+            _dbService.UpdateEntity(candidate);
+        }
+    }
+
+    private bool ValidateVoteInput()
+    {        
+        txtVotesToAdd.Text = txtVotesToAdd.Text.Trim();
+
+        if (string.IsNullOrWhiteSpace(txtVotesToAdd.Text))
+        {
+            MessageBox.Show("Please enter number of votes to add", "Add votes", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            txtVotesToAdd.BackColor = Color.Red;
+            return false;
+        }
+
+        if (int.TryParse(txtVotesToAdd.Text, out int result))
+        {
+            if (result > 0)
+                return true;
+
+            return false;
+        }
+        else
+        {
+            MessageBox.Show("Please enter a number", "Add votes", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            return false;
+        }
     }
 }
