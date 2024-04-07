ec71faaf480f0a41d7d475825b8db7029e8f1786
fM4s0n <115559911+fM4s0n@users.noreply.github.com>
2024-04-07 22:51:09 +0100

    cleaned up election facvtory and added Preferential electio class

--- VotifySystem/Common/Models/Elections/ElectionFactory.cs
+++ VotifySystem/Common/Models/Elections/ElectionFactory.cs
@@ -7,47 +7,77 @@ namespace VotifySystem.Common.Models.Elections;
 /// </summary>
 public static class ElectionFactory
 {
+    /// <summary>
+    /// Basic creation of an election
+    /// used for creating new election for frmCreateElection where the id is needed early
+    /// </summary>
     public static Election CreateElection(ElectionVoteMechanism voteMechanism, Country country)
     {
-        return voteMechanism switch
+        switch (voteMechanism)
         {
-            ElectionVoteMechanism.FPTP => new FirstPastThePostElection
-            {
-                ElectionId = Guid.NewGuid().ToString(),
-                Country = country
-            },
-            ElectionVoteMechanism.STV => new SingleTransferrableVoteElection
-            {
-                ElectionId = Guid.NewGuid().ToString(),
-                Country = country
-            },
-            _ => throw new NotImplementedException()
-        };
+            case ElectionVoteMechanism.FPTP:
+                return new FirstPastThePostElection
+                {
+                    Country = country,
+                    ElectionId = Guid.NewGuid().ToString()
+                };
+            case ElectionVoteMechanism.STV:
+                return new SingleTransferrableVoteElection
+                {
+                    Country = country,
+                    ElectionId = Guid.NewGuid().ToString()
+                };
+            case ElectionVoteMechanism.Preferential:
+                return new PreferentialElection
+                {
+                    Country = country,
+                    ElectionId = Guid.NewGuid().ToString()
+                };
+            default:
+                throw new NotImplementedException();
+        }
     }
 
+    /// <summary>
+    /// Creates with all parameters specified
+    /// Used for data seeding
+    /// </summary>
     public static Election CreateElection(ElectionVoteMechanism voteMechanism, Country country, string description, DateTime startDate, DateTime endDate, string userId)
     {
-        return voteMechanism switch
+        switch (voteMechanism)
         {
-            ElectionVoteMechanism.FPTP => new FirstPastThePostElection
-            {
-                Country = country,
-                ElectionId = Guid.NewGuid().ToString(),
-                Description = description,
-                StartDate = startDate,
-                EndDate = endDate,
-                ElectionAdministratorId = userId
-            },
-            ElectionVoteMechanism.STV => new SingleTransferrableVoteElection
-            {
-                Country = country,
-                ElectionId = Guid.NewGuid().ToString(),
-                Description = description,
-                StartDate = startDate,
-                EndDate = endDate,
-                ElectionAdministratorId = userId
-            },
-            _ => throw new NotImplementedException()
-        };           
+            case ElectionVoteMechanism.FPTP:
+                return new FirstPastThePostElection
+                {
+                    Country = country,
+                    ElectionId = Guid.NewGuid().ToString(),
+                    Description = description,
+                    StartDate = startDate,
+                    EndDate = endDate,
+                    ElectionAdministratorId = userId
+                };
+            case ElectionVoteMechanism.STV:
+                return new SingleTransferrableVoteElection
+                {
+                    Country = country,
+                    ElectionId = Guid.NewGuid().ToString(),
+                    Description = description,
+                    StartDate = startDate,
+                    EndDate = endDate,
+                    ElectionAdministratorId = userId
+                };
+            case ElectionVoteMechanism.Preferential:
+                return new PreferentialElection
+                {
+                    Country = country,
+                    ElectionId = Guid.NewGuid().ToString(),
+                    Description = description,
+                    StartDate = startDate,
+                    EndDate = endDate,
+                    ElectionAdministratorId = userId
+                };
+            default:
+                throw new NotImplementedException();
+        }
     }
 }
---
+++ VotifySystem/Common/Models/Elections/PreferentialElection.cs
@@ -0,0 +1,10 @@
+﻿namespace VotifySystem.Common.Models.Elections;
+
+public class PreferentialElection : Election
+{
+    public readonly ElectionVoteMechanism VoteMechanism = ElectionVoteMechanism.Preferential;
+
+    public PreferentialElection()
+    {
+    }
+}
